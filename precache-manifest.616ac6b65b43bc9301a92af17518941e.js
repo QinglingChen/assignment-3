3e17b0f58ad896f6b281e62a760dc117da334270
QinglingChen <qingling.chen75@myhunter.cuny.edu>
2024-12-06 03:33:38 -0500

    Updates

---
+++ precache-manifest.616ac6b65b43bc9301a92af17518941e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9beef7676bc377eafb14b6b949d9501d",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "e47ef55fd5679f436ad8",
+    "url": "/my-react-app/static/css/main.09be3d56.chunk.css"
+  },
+  {
+    "revision": "4063feaee8225f4741a1",
+    "url": "/my-react-app/static/js/2.766b93b6.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/my-react-app/static/js/2.766b93b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c55b0ae3e9a88e89b10",
+    "url": "/my-react-app/static/js/3.f0df6801.chunk.js"
+  },
+  {
+    "revision": "e47ef55fd5679f436ad8",
+    "url": "/my-react-app/static/js/main.3e7d2a3b.chunk.js"
+  },
+  {
+    "revision": "a9674ece6f6665b3a84b",
+    "url": "/my-react-app/static/js/runtime-main.a839ecfd.js"
+  }
+]);
